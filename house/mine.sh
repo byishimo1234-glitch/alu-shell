de4356708f178ce518e36e31f6b49d02494695ee
byishimo123-glitch <j.byishimo@alustudent.com>
2025-02-16 11:40:52 -0800

    add all

---
+++ house/mine.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+#create a directory
+mkdir name
+
+#create files inside the directory
+touch name/file1.txt  name/file2.txt
+
+#Add content to the file
+echo "I like this file" > name/file1.txt
+echo "This is fun" > namefile2.txt
